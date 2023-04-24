e5c04ec424908bafcde4b4824804ff7135a78476
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-04-24 10:30:09 -0500

    Test models done

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,85 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @employee = employees(:employee_one)
+    @payroll_period = payroll_periods(:payroll_period_one)
+  end
+
+  test 'colums valid is valid' do
+    assert Employee.column_names.include?('base_salary'), 'column base_salary is included in Employee'
+    assert_equal 'integer', employee_column_class('base_salary').type.to_s, 'base_salary column is a float'
+    assert_not employee_column_class('base_salary').null, 'name column is not null'
+
+    assert Employee.column_names.include?('name'), 'column name is included in Employee'
+    assert_equal 'string', employee_column_class('name').type.to_s, 'name column is a string'
+    assert_not employee_column_class('name').null, 'name column is not null'
+
+    assert Employee.column_names.include?('document_identity'), 'column document_identity is included in Employee'
+    assert_equal 'integer', employee_column_class('document_identity').type.to_s, 'document_identity column is a string'
+    assert_not employee_column_class('document_identity').null, 'document_identity column cant be null'
+
+    assert Employee.column_names.include?('payroll_period_id'), 'column payroll_period_id is included in Employee'
+    assert_equal 'integer', employee_column_class('payroll_period_id').type.to_s,
+                 'payroll_period_id column is an integer'
+    assert_not employee_column_class('payroll_period_id').null, 'payroll_period_id column cant be null'
+
+    assert Employee.column_names.include?('percentage_of_social_security'),
+           'column percentage_of_social_security is included in Employee'
+    assert_equal 'float', employee_column_class('percentage_of_social_security').type.to_s,
+                 'percentage_of_social_security column is a float'
+    assert employee_column_class('percentage_of_social_security').null,
+           'percentage_of_social_security column can be null'
+
+    assert Employee.column_names.include?('percentage_of_pension_fund'),
+           'column percentage_of_pension_fund is included in Employee'
+    assert_equal 'float', employee_column_class('percentage_of_pension_fund').type.to_s,
+                 'percentage_of_pension_fund column is a float'
+    assert employee_column_class('percentage_of_pension_fund').null, 'percentage_of_pension_fund column can be null'
+
+    assert Employee.column_names.include?('solidarity_fund'), 'column solidarity_fund is included in Employee'
+    assert_equal 'float', employee_column_class('solidarity_fund').type.to_s, 'solidarity_fund column is a float'
+    assert employee_column_class('solidarity_fund').null, 'solidarity_fund column can be null'
+
+    assert Employee.column_names.include?('subsistence_fund'), 'column subsistance_fund is included in Employee'
+    assert_equal 'float', employee_column_class('subsistence_fund').type.to_s, 'subsistance_fund column is a float'
+    assert employee_column_class('subsistence_fund').null, 'subsistance_fund column can be null'
+
+    assert Employee.column_names.include?('total_deductions'), 'column total_deductions is included in Employee'
+    assert_equal 'integer', employee_column_class('total_deductions').type.to_s, 'total_deductions column is a float'
+    assert employee_column_class('total_deductions').null, 'total_deductions column can be null'
+
+    assert Employee.column_names.include?('non_salary_income'), 'column non_salary_income is included in Employee'
+    assert_equal 'integer', employee_column_class('non_salary_income').type.to_s, 'non_salary_income column is a float'
+    assert employee_column_class('non_salary_income').null, 'non_salary_income column can be null'
+
+    assert Employee.column_names.include?('other_salary_income'), 'column other_salary_income is included in Employee'
+    assert_equal 'integer', employee_column_class('other_salary_income').type.to_s,
+                 'other_salary_income column is a float'
+    assert employee_column_class('other_salary_income').null, 'other_salary_income column can be null'
+  end
+
+  test 'employee has a relationship with payroll_period' do
+    assert_equal @employee.payroll_period, @payroll_period, 'employee has a relationship with payroll_period'
+  end
+
+  test 'employee is created correctly' do
+    assert @employee.valid?, 'employee is valid'
+  end
+
+  test 'employee cant be created with nill params' do
+    employee = Employee.new
+    assert_not employee.save, 'employee is not valid'
+  end
+
+  test 'employee cant be created with unsafe params' do
+    employee = Employee.new(name: '@@++', document_identity: '@@++', payroll_period_id: '++@@', base_salary: '@@**')
+    assert_not employee.save, 'employee is not valid'
+  end
+
+  def employee_column_class(column)
+    Employee.columns.detect { |c| c.name == column.to_s }
+  end
 end
--- test/models/payroll_period_test.rb
+++ test/models/payroll_period_test.rb
@@ -1,7 +1,87 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PayrollPeriodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = companies(:company_one)
+    # @faild_payroll_period = payroll_periods(:payroll_period_two)
+    @payroll_period = payroll_periods(:payroll_period_one)
+  end
+
+  test 'columns are valid is valid' do
+    assert PayrollPeriod.column_names.include?('start_date'), 'column start_date is included in PayrollPeriod'
+    assert_equal 'date', payroll_period_column_class('start_date').type.to_s, 'start_date column is a date'
+    assert_not payroll_period_column_class('start_date').null, 'start_date column cant be null'
+
+    assert PayrollPeriod.column_names.include?('end_date'), 'column end_date is included in PayrollPeriod'
+    assert_equal 'date', payroll_period_column_class('end_date').type.to_s, 'end_date column is a date'
+    assert_not payroll_period_column_class('end_date').null, 'end_date column cant be null'
+
+    assert PayrollPeriod.column_names.include?('company_id'), 'column company_id is included in PayrollPeriod'
+    assert_equal 'integer', payroll_period_column_class('company_id').type.to_s, 'company_id column is an integer'
+    assert_not payroll_period_column_class('company_id').null, 'company_id column cant be null'
+
+    assert PayrollPeriod.column_names.include?('percentage_of_social_security'),
+           'column percentage_of_social_security is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('percentage_of_social_security').type.to_s,
+                 'percentage_of_social_security column is a float'
+    assert payroll_period_column_class('percentage_of_social_security').null,
+           'percentage_of_social_security column can be null'
+
+    assert PayrollPeriod.column_names.include?('percentage_of_pension_fund'),
+           'column percentage_of_pension_fund is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('percentage_of_pension_fund').type.to_s,
+                 'percentage_of_pension_fund column is a float'
+    assert payroll_period_column_class('percentage_of_pension_fund').null,
+           'percentage_of_pension_fund column can be null'
+
+    assert PayrollPeriod.column_names.include?('percentage_arl'), 'column percentage_arl is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('percentage_arl').type.to_s, 'percentage_arl column is a float'
+    assert payroll_period_column_class('percentage_arl').null, 'percentage_arl column can be null'
+
+    assert PayrollPeriod.column_names.include?('compensation'), 'column compensation is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('compensation').type.to_s, 'compensation column is a float'
+    assert payroll_period_column_class('compensation').null, 'compensation column can be null'
+
+    assert PayrollPeriod.column_names.include?('icbf'), 'column icbf is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('icbf').type.to_s, 'icbf column is a float'
+    assert payroll_period_column_class('icbf').null, 'icbf column can be null'
+
+    assert PayrollPeriod.column_names.include?('sena'), 'Column sena is included in PayrollPeriod'
+    assert_equal 'float', payroll_period_column_class('sena').type.to_s, 'sena column is a float'
+    assert payroll_period_column_class('sena').null, 'sena column can be null'
+
+    assert PayrollPeriod.column_names.include?('minimum_salary'), 'Column minimum_salary is included in PayrollPeriod'
+    assert_equal 'integer', payroll_period_column_class('minimum_salary').type.to_s, 'minimum_salary column is a float'
+    assert payroll_period_column_class('minimum_salary').null, 'minimum_salary column can be null'
+
+    assert PayrollPeriod.column_names.include?('transport_subsidy'),
+           'Column transport_subsidy is included in PayrollPeriod'
+    assert_equal 'integer', payroll_period_column_class('transport_subsidy').type.to_s,
+                 'transport_subsidy column is a float'
+    assert payroll_period_column_class('transport_subsidy').null, 'transport_subsidy column can be null'
+  end
+
+  test 'payroll_period have a relationship with a company' do
+    assert_equal companies(:company_one), @payroll_period.company
+  end
+
+  test 'payroll_period is created with valid params' do
+    assert @payroll_period.save, 'Payroll period was saved'
+  end
+
+  test 'payroll_period is not created with nill params' do
+    @payroll_period.company_id = nil
+    assert_not @payroll_period.save, 'Payroll period cant be saved with nill params'
+  end
+
+  test 'payroll_period cant be created with unsafe params' do
+    payroll_period = PayrollPeriod.new(company_id: '**@@', start_date: '++@', end_date: '++@')
+    assert_not payroll_period.save, 'Payroll period cant be saved with unsafe params'
+  end
+
+  def payroll_period_column_class(column)
+    PayrollPeriod.columns.detect { |c| c.name == column.to_s }
+  end
 end
