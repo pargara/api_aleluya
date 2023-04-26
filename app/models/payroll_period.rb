30fb9423423bc8d388b13b86990eca4c66126cc2
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2023-04-26 16:04:44 -0500

    Adding validation for 30 days calendar

--- app/models/payroll_period.rb
+++ app/models/payroll_period.rb
@@ -12,4 +12,15 @@ class PayrollPeriod < ApplicationRecord
   validates :end_date, presence: true
   validates :start_date, :end_date,
             format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'El formato debe de ser (YYYY-MM-DD)' }
+  validate :validate_date_range
+
+  private
+
+  def validate_date_range
+    @period = start_date..end_date
+    @range = 28..31
+    if !@range.include?(@period.count)
+      errors.add(:end_date, "must be at least 28 days after start date")
+    end
+  end
 end
